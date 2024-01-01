c627c24c7ad9aff93daeb102177cc3bd34c1a103
siyush umath <siyush52@gmail.com>
2024-01-01 16:39:06 +0530

    pattern

---
+++ solidrectangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the value of n :- ");
+    scanf("%d",&n);
+    int m;
+    printf("Enter the value of m :- ");
+    scanf("%d",&m);
+    for(int i=1;i<=n;i++){
+        for(int i=1;i<=m;i++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
